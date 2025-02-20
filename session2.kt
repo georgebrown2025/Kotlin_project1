3239112fb1c1e91c7a48f76b80977fe443ca1179
Arjun <arjunbishnoi1@gmail.com>
2025-02-19 21:22:01 -0500

    Final code for Session2

--- session2.kt
+++ session2.kt
@@ -3,6 +3,8 @@
 // 2. Use the template to create individual objects - INSTANCES (OBJECTS)
 
 
+
+
 // Update the template so you can customize the properties to you suit your individual needs
 class Car {
     // define the properties of a car
@@ -19,24 +21,91 @@ class Car {
     var price:Double
 
 
+
+
+    var speed:Int = 0
+
+
+    fun accelerate(speed:Int) {
+        println("The car is increasing speed!")
+        this.speed += speed
+    }
+    fun brake() {
+        println("The car is stopped!")
+        speed = 0
+    }
+
+
     //  special function to your class called a constructor
     // Customize the default value of your properties
-    constructor(lp:String, c:String, p:Double) {
+    constructor(lp:String, c:String) {
         println("++ DEBUG: Constructor #1 function is executing")
         licensePlate = lp
         color = c
-        price = p
+
+
+        if (color === "blue") {
+            price = 1000.0 - 125.0
+        } else if (color === "orange") {
+            price = 1000.0 * 1.25
+        }
+        else {
+            price = 1000.0
+        }
+
+
         println("++ DEBUG: Constructor #1 function is done!")
     }
 
 
     // classes can have MORE THAN ONE constructor
-    constructor(lp:String, p:Double) {
+    constructor(lp:String) {
         println("++ DEBUG: Constructor #2 function is executing")
         color = "orange"
         licensePlate = lp
-        price = p
+
+
+        price = 1000.0 * 1.25
+
+
         println("++ DEBUG: Constructor #2 function is done!")
+    }
+
+
+    override fun toString(): String {
+        return "Car(model='$model', color='$color', licensePlate='$licensePlate', price=$price, speed=$speed)"
+    }
+
+
+
+
+}
+
+
+
+
+
+
+class Book {
+    // properties
+    var title:String
+    var author:String
+    var yearPublished:Int
+    var numPage:Int
+    var price:Double
+
+
+    constructor(title: String, author: String, yearPublished: Int, numPage: Int, price: Double) {
+        this.title = title
+        this.author = author
+        this.yearPublished = yearPublished
+        this.numPage = numPage
+        this.price = price
+    }
+
+
+    override fun toString(): String {
+        return "Book(title='$title', author='$author', yearPublished=$yearPublished, numPage=$numPage, price=$price)"
     }
 
 
@@ -49,30 +118,49 @@ fun main() {
     println("DEMO OF CLASSES")
     // Using the class, create individual cars
     // Variable of type Car
-    val c1: Car = Car("HND101", "black", 1000.0)
+    val c1:Car = Car("HND101", "black")
     println("car created!")
-    // output the properties of your car using dot notation
-    println(c1.model)
-    println(c1.licensePlate)
-    println(c1.color)
-    println(c1.price)
 
 
     // orange by default
     // option 1: constructor #1
-    val c2: Car = Car("CIV999", "orange", 1000.0 * 1.25)
-    println("-----car 2 created----")
-    println(c2.model)
-    println(c2.licensePlate)
-    println(c2.color)
-    println(c2.price)
-    println("-----car 3 created----")
+    val c2:Car = Car("CIV999", "orange")
+    println("-----car 2 created: orange car with constructor #1----")
+
+
+
+
+    println("-----car 3 created: orange car with constructor #2----")
     // option 2: use constructor #2
+    val c3:Car = Car("HC131", "orange")
 
 
-    val c3: Car = Car("HC131", "blue", 1000.0 - 125)
-    println(c3.model)
-    println(c3.licensePlate)
-    println(c3.color)
-    println(c3.price)
+
+
+    println("-----car 4 created----")
+    val c4:Car = Car("HC131", "blue")
+
+
+    println(c1)         //toString()
+    println(c2.toString())
+    println(c3)
+    println(c4)
+
+
+
+
+    var b1:Book = Book("Harry Potter", "JK Rowling", 1997, 200, 21.99)
+    println(b1)
+
+
+    println(c1)
+    c1.accelerate(10)         // +5
+    c1.accelerate(7)         // +5
+    c1.accelerate(3)         // +5
+    c1.accelerate(50)         // +5
+    println(c1)
+    c1.brake()
+    println(c1)
+
+
 }
