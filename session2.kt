91215d735ce9d0b1dccbf81ee24f983b6057565e
Arjun <arjunbishnoi1@gmail.com>
2025-02-19 20:43:46 -0500

    Add price variables to class car

--- session2.kt
+++ session2.kt
@@ -1,9 +1,13 @@
+// to build  real world objects
+// 1. Template/ blueprint for what your object should look like - CLASS
+// 2. Use the template to create individual objects - INSTANCES (OBJECTS)
+
+
+// Update the template so you can customize the properties to you suit your individual needs
 class Car {
     // define the properties of a car
     // variables inside the class - explicit variable declaration
     // and you must provide a default value for those variables
-
-
     var model:String = "Honda Civic"
     // OPTION 1: Is the default value of the color going to be the same for all cars?
     // var color:String = "black"
@@ -12,21 +16,26 @@ class Car {
     var licensePlate:String
 
 
+    var price:Double
+
+
     //  special function to your class called a constructor
     // Customize the default value of your properties
-    constructor(lp:String, c:String) {
+    constructor(lp:String, c:String, p:Double) {
         println("++ DEBUG: Constructor #1 function is executing")
         licensePlate = lp
         color = c
+        price = p
         println("++ DEBUG: Constructor #1 function is done!")
     }
 
 
-    // classes can have MORE than one constructors
-    constructor(lp:String) {
+    // classes can have MORE THAN ONE constructor
+    constructor(lp:String, p:Double) {
         println("++ DEBUG: Constructor #2 function is executing")
         color = "orange"
         licensePlate = lp
+        price = p
         println("++ DEBUG: Constructor #2 function is done!")
     }
 
@@ -40,29 +49,30 @@ fun main() {
     println("DEMO OF CLASSES")
     // Using the class, create individual cars
     // Variable of type Car
-    val c1:Car = Car("HND101", "black")
+    val c1: Car = Car("HND101", "black", 1000.0)
     println("car created!")
     // output the properties of your car using dot notation
     println(c1.model)
     println(c1.licensePlate)
     println(c1.color)
+    println(c1.price)
 
 
     // orange by default
     // option 1: constructor #1
-    val c2:Car = Car("CIV999", "orange")
+    val c2: Car = Car("CIV999", "orange", 1000.0 * 1.25)
     println("-----car 2 created----")
     println(c2.model)
     println(c2.licensePlate)
     println(c2.color)
+    println(c2.price)
     println("-----car 3 created----")
     // option 2: use constructor #2
-    val c3:Car = Car("HC131")
+
+
+    val c3: Car = Car("HC131", "blue", 1000.0 - 125)
     println(c3.model)
     println(c3.licensePlate)
     println(c3.color)
-
-
-
-
+    println(c3.price)
 }
