988c9394e1df9485b1ccc48870248383022a8dad
Arjun <arjunbishnoi1@gmail.com>
2025-02-22 19:36:57 -0500

    Basic val and var and string interpolation

--- index.kt
+++ index.kt
@@ -1,5 +1,19 @@
-
+fun sayhello() {
+    println("Hello, how are you?")
+    println("this is me saying hello to you!")
+}
 
 fun main() {
+    println("testing!")
+    sayhello()
+    println(3+4-1)
+
+    val x = 25
+    var y = 100
+    y = 101
+
+    println("What is the number x? : ${x}")
+    println("What is the number y? :${y}")
+    println("What is the sum of x and y? ${x+y}")
 
 }
